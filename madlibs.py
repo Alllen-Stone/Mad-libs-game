ec0e0ec846827e9c9b35a27d14e27ebd31ec38c3
AllenStone2000 <73773540+Alllen-Stone@users.noreply.github.com>
2020-11-06 15:51:39 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+# A MadLIBS Game
+print("Welocome to Mad_____Libs")
+print("Greatest word game")
+
+adj1  =input("Enter a adjective: ")
+adj2  =input("Enter another adjevtive: ")
+bird  =input("Enter the name of your favourite bird: ")
+room  =input("Enter a name of a room: ")
+verb1 =input("Enter a verb(past tense): ")
+verb2 =input("Enter a verb: ")
+verb3 =input("Give me another verb ending with 'ing': ")
+verb4 =input("Type a another one ending with 'ing': ")
+name1 =input("Who is favourite relative?: ")
+noun1 =input("Enter a noun: ")
+noun2 =input("Type a plural noun: ")
+noun3 =input("Give me another noun: ")
+liquid=input("Enter a liquid: ")
+body_part =input("Name a body part: ")
+
+
+print("It was a " + adj1 + ", cold November day. I woke up to the " + adj2 + " smell of\n" 
+     + bird + " roasting in the " + room + " downstairs. I " + verb1 + " down the stairs\n"
++ "to see if I could help " + verb2 + " the dinner. My mom said,'See if your " + name1 +"\n" + 
+"needs a fresh " + noun1 + ".' So I carried a tray of glasses full of " + liquid + " into the\n"
++ verb3 + " room. When I got there, I couldn't believe my " + body_part + "!. There were " + noun2 +"\n" 
++ verb4 + " on the " + noun3 + ".") 
+
